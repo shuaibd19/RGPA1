9aa2c5a17c28bf1d6e6ce3cf01866a87b5084900
Ahmad Dorani <Ahmad.Dorani@mds.torrens.edu.au>
2021-06-09 20:27:17 +1000

    updated it so force is cumulative

--- Normy and Shuaib RGP/Assets/Scripts/BowShoot.cs
+++ Normy and Shuaib RGP/Assets/Scripts/BowShoot.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// script to control bow shooting arrows
@@ -10,13 +11,16 @@ public class BowShoot : MonoBehaviour
     [SerializeField] private Camera cam;
     [SerializeField] private GameObject arrow;
     [SerializeField] private Transform ejectionPoint;
+    [SerializeField] private Text forceGuage;
 
-    //[SerializeField] private float testForce = 20f;
+    [SerializeField] private float testForce = 0.3f;
 
-    [SerializeField] private float shootForce = 10000f;
+    //[SerializeField] private float shootForce = 60f;
 
-    [SerializeField] private float maxForce = 30f;
-    [SerializeField] private float minForce = 1;
+    [SerializeField] private float maxForce = 50f;
+    [SerializeField] private float minForce = 0f;
+
+    private float vertical = 0f;
 
     //the guage meter for the amount of force to exert on arrow based on mouse y axis movement
     float guage = 0f;
@@ -24,9 +28,14 @@ public class BowShoot : MonoBehaviour
     private void Update()
     {
         //make this an absolute value
-        var vertical = Mathf.Abs(Input.GetAxis("Mouse Y") * shootForce);
+        //var vertical = Mathf.Abs(Input.GetAxis("Mouse Y") * shootForce);
+
+        //vertical += Mathf.Abs(Input.GetAxis("Mouse Y") * testForce);
+        vertical += (Input.GetAxis("Mouse Y") * -1f) * testForce;
         //clamp the result between the maxforce and min force and assign it to the guage
         guage = Mathf.Clamp(vertical, minForce, maxForce);
+
+        forceGuage.text = "Force: " + guage.ToString();
 
         //Debug.Log(vertical);
 
@@ -35,6 +44,7 @@ public class BowShoot : MonoBehaviour
             GameObject arr = Instantiate(arrow, ejectionPoint.position, Quaternion.identity);
             Rigidbody rb = arr.GetComponent<Rigidbody>();
             rb.velocity = cam.transform.forward * guage;
+            guage = 0f;
         }
 
 
