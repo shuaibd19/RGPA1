74da296288fea1c6bd77020d54b828a3243fe483
Ahmad Dorani <Ahmad.Dorani@mds.torrens.edu.au>
2021-06-09 19:23:39 +1000

    added bow script

---
+++ Normy and Shuaib RGP/Assets/Scripts/BowShoot.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// script to control bow shooting arrows
+/// </summary>
+public class BowShoot : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private GameObject arrow;
+    [SerializeField] private Transform ejectionPoint;
+
+    [SerializeField] private float shootForce = 10000f;
+
+    [SerializeField] private float maxForce = 30f;
+    [SerializeField] private float minForce = 1;
+
+    //the guage meter for the amount of force to exert on arrow based on mouse y axis movement
+    float guage = 0f;
+
+    private void FixedUpdate()
+    {
+        //make this an absolute value
+        var vertical = Mathf.Abs(Input.GetAxis("Mouse Y") * Time.deltaTime  * shootForce);
+        //clamp the result between the maxforce and min force and assign it to the guage
+        guage = Mathf.Clamp(vertical, minForce, maxForce);
+
+        //Debug.Log(vertical);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject arr = Instantiate(arrow, ejectionPoint.position, Quaternion.identity);
+            Rigidbody rb = arr.GetComponent<Rigidbody>();
+            rb.velocity = cam.transform.forward * guage;
+        }
+
+
+    }
+}
