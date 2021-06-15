1b64002c6bc4a8b7a188ce2012356166596dbf5b
Normy Haddad <normy.haddad@gmail.com>
2021-06-15 11:16:24 +1000

    Fixed Catapult

--- Normy and Shuaib RGP/Assets/Catapult.cs
+++ Normy and Shuaib RGP/Assets/Catapult.cs
@@ -5,38 +5,71 @@ using UnityEngine;
 public class Catapult : MonoBehaviour
 {
     public GameObject catapultArm;
+    public GameObject projectile;
+
     public float windingSpeed;
+    public float unwindCoefficient;
 
     float tension;
+    float mouseStartY;
+    float unwindSpeed;
+
+    float minAngle;
+    float maxAngle;
 
     bool unloading;
+    bool canFire = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mouseStartY = Input.mousePosition.y;
+        minAngle = 0.97f;
+        maxAngle = 0.79f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.S))
+        if (Input.mousePosition.y < mouseStartY && catapultArm.transform.rotation.y >= maxAngle && !unloading)
         {
-            catapultArm.transform.Rotate(new Vector3(1, 0, 0), -windingSpeed * Time.deltaTime);
-            tension += 10 * Time.deltaTime;
-            print(catapultArm.transform.rotation.y);
+            float diff = Mathf.Abs(Input.mousePosition.y - mouseStartY);
+            catapultArm.transform.Rotate(new Vector3(1, 0, 0), (Input.mousePosition.y - mouseStartY) * diff / windingSpeed);
+            //tension += 24 * diff / windingSpeed;
         }
+        mouseStartY = Input.mousePosition.y;
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
             unloading = true;
+            unwindSpeed = minAngle / Mathf.Pow((catapultArm.transform.rotation.y / 2), 2);
         }
 
-        //why won't it stop
         if (unloading)
-            catapultArm.transform.Rotate(new Vector3(1, 0, 0), tension * Time.deltaTime);
+        {
+            catapultArm.transform.Rotate(new Vector3(1, 0, 0), unwindSpeed * unwindCoefficient * Time.deltaTime);
+        }
 
-        if (catapultArm.transform.rotation.y >= 0.96)
-            //tension = 0; //for some reason this line breaks the whole thing
+        if (catapultArm.transform.rotation.y > minAngle)
+        {
             unloading = false;
+            canFire = true;
+        }
+
+        if (canFire && projectile != null)
+        {
+            LaunchObj();
+            projectile = null;
+        }
+    }
+
+    void LaunchObj()
+    {
+        //min wind = 0.96, max wind = 0.79
+        Rigidbody projRB = projectile.GetComponent<Rigidbody>();
+        projectile.transform.SetParent(null);
+        projRB.isKinematic = false;
+        projRB.useGravity = true;
+        projRB.AddRelativeForce(Vector3.forward * unwindSpeed * unwindCoefficient * 2);
+        projectile.GetComponent<despawnTimer>().StartTimer();
     }
 }
---
+++ Normy and Shuaib RGP/Assets/despawnTimer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class despawnTimer : MonoBehaviour
+{
+    public float timeToDespawn;
+
+    float timer;
+    bool countdown = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = timeToDespawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (countdown)
+            timer -= Time.deltaTime;
+
+        if (timer <= 0.0f)
+            gameObject.SetActive(false);
+
+    }
+
+    public void StartTimer()
+    {
+        countdown = true;
+        timer = timeToDespawn;
+    }
+}
